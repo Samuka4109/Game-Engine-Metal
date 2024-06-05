889d6a2c9a82b181d650c9438d21461a5b5677dc
Samuka4109 <samuel.parnaiba@gmail.com>
2024-06-05 19:34:32 -0300

    Adição de código em comentario para acressentar mais elementos em tela

--- Game Engine/Game Shizz/Scenes/SandboxScene.swift
+++ Game Engine/Game Shizz/Scenes/SandboxScene.swift
@@ -13,17 +13,22 @@ class SandboxScene: Scene{
     }
     
     var cubeCollection1: CubeCollection!
+    //var cubeCollection2: CubeCollection!
+
     func addCubes(){
         
         cubeCollection1 = CubeCollection(cubesWide: 20, cubesHigh: 20, cubesBack: 20)
-       // cubeCollection1.position.x = -16
+        cubeCollection1.position.x = 0
         addChild(cubeCollection1)
         
+       /* cubeCollection2 = CubeCollection(cubesWide: 20, cubesHigh: 20, cubesBack: 20)
+        cubeCollection2.position.x = 16
+        addChild(cubeCollection2)*/
     }
     
     override func update(deltaTime: Float) {
         cubeCollection1.rotation.z += deltaTime
-        
+       // cubeCollection2.rotation.z += deltaTime
         super.update(deltaTime: deltaTime)
     }
 }
